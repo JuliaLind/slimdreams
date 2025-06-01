018eb4341f56bb14e68b467ec2030a2d086142d1
Julia Lind <jl225vf@student.lnu.se>
2025-06-01 18:57:31 +0200

    Modify test script to build app before running test:e2e command

--- test.bash
+++ test.bash
@@ -10,8 +10,11 @@ npm run test:scen
 
 cd ../web-server
 npm run test:unit
+
+# build the application before running test:e2e
+npm run build
 npm run test:e2e # Runs e2e tests of built version without the interative Cypress GUI
 
-# npm run test:e2e:dev # Uncomment this line to run the tests in interactive mode using Cypress GUI
+# npm run test:e2e:dev # Uncomment this line to run the tests in interactive mode using Cypress GUI. This line is running the tests from the source code, not the built version.
 
 cd ..
